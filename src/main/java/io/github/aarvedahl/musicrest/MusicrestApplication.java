9660ab913e3a417b4a906e7c9432cedc616b2f01
Alex <alex.arvedahl@gmail.com>
2017-12-04 20:38:16 +0100

    Added project files and dependencies

---
+++ src/main/java/io/github/aarvedahl/musicrest/MusicrestApplication.java
@@ -0,0 +1,12 @@
+package io.github.aarvedahl.musicrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicrestApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/aarvedahl/musicrest/MusicrestApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.aarvedahl.musicrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicrestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
