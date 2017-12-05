a5e35405e6bc43fdd295af6380b59251b6f4a2f4
Alex <alex.arvedahl@gmail.com>
2017-12-05 15:21:37 +0100

    Sort on rating working

--- src/main/java/io/github/aarvedahl/musicrest/controllers/AlbumController.java
+++ src/main/java/io/github/aarvedahl/musicrest/controllers/AlbumController.java
@@ -3,11 +3,15 @@ package io.github.aarvedahl.musicrest.controllers;
 import io.github.aarvedahl.musicrest.model.Album;
 import io.github.aarvedahl.musicrest.model.Song;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class AlbumController {
-
+// primitiva datatyper som är static hamnar på native minnet
+    // En lista är en collection men collection är inte en lista
+    // Collection är superklass till set, map, list etc
     public List<Song> showFavorites(List<Album> albums) {
         List<Song> songList = new LinkedList<>();
         for(Album album: albums) {
@@ -23,8 +27,21 @@ public class AlbumController {
     public List<Song> sortOnRating(List<Album> albumList) {
         // Visa max ev. 25 st
         List<Song> bestRatings = new LinkedList<>();
-
-        return albumList.get(0).getSongs();
+        for(Album album: albumList) {
+            for(Song song: album.getSongs()) {
+                bestRatings.add(song);
+            }
+        }
+        bestRatings.sort(new RatingComparator());
+        return bestRatings;
     }
+
+    class RatingComparator implements Comparator<Song> {
+        @Override
+        public int compare(Song a, Song b) {
+            return a.getRating() > b.getRating() ? -1 : a.getRating() == b.getRating() ? 0 : 1;
+        }
+    }
+
 
 }
--- src/test/java/io/github/aarvedahl/musicrest/AlbumControllerTest.java
+++ src/test/java/io/github/aarvedahl/musicrest/AlbumControllerTest.java
@@ -13,39 +13,32 @@ import java.util.List;
 public class AlbumControllerTest {
     AlbumController albumController;
     List<Album> albumList;
+
     @Before
     public void init() {
         albumController = new AlbumController();
         albumList = new ArrayList<>();
         Album album = new Album("Taylor Swift", "Red", "Pop", "url");
-        Song song = new Song("I almost do", false, 3);
+        Song song = new Song("I knew you were trouble", false, 4);
         album.addSong(song);
-        song = new Song("We are never ever getting back together", true, 5);
+        song = new Song("I almost do", false, 5);
+        album.addSong(song);
+        song = new Song("We are never ever getting back together", true, 2);
         album.addSong(song);
         albumList.add(album);
 
     }
 
-
     @Test
     public void showFavorite() {
-        Album album = new Album("Taylor Swift", "Red", "Pop", "url");
-        Song song = new Song("We are never ever getting back together", true, 2);
-        album.addSong(song);
-        assertEquals(album.getSongs().get(0).getSongTitle(), albumController.showFavorites(albumList).get(0).getSongTitle());
         for(Song song1: albumController.showFavorites(albumList)) {
             assertEquals(true, song1.isFavorite());
         }
     }
 
-
     @Test
     public void sortOnRating() {
-        Album album = new Album("Taylor Swift", "Red", "Pop", "url");
-        Song song = new Song("I almost do", false, 3);
-        album.addSong(song);
-        song = new Song("We are never ever getting back together", true, 5);
-        album.addSong(song);
-        assertEquals(album.getSongs().get(0).getSongTitle(), albumController.sortOnRating(albumList).get(0).getSongTitle());
+        assertEquals("I almost do", albumController.sortOnRating(albumList).get(0).getSongTitle());
+        assertEquals("I knew you were trouble", albumController.sortOnRating(albumList).get(1).getSongTitle());
     }
 }
